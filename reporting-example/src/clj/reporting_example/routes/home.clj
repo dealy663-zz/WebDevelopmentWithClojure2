fe2b4c6f646458c907aafe1ee433a4bc77de0d62
dealy663 <dealy663@gmail.com>
2017-04-29 16:38:44 -0700

    added PDF creation logic and completed exercise

--- reporting-example/src/clj/reporting_example/routes/home.clj
+++ reporting-example/src/clj/reporting_example/routes/home.clj
@@ -2,16 +2,36 @@
   (:require [reporting-example.layout :as layout]
             [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :as response]
+            [reporting-example.reports :as reports]
+            [reporting-example.layout :as layouts]
             [clojure.java.io :as io]))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html"))
 
 (defn about-page []
   (layout/render "about.html"))
 
+(defn write-response [report-bytes]
+  (with-open [in (java.io.ByteArrayInputStream. report-bytes)]
+    (-> (response/ok in)
+        (response/header "Content-Disposition" "filename=document.pdf")
+        (response/header "Content-Length" (count report-bytes))
+        (response/content-type "application/pdf")) ))
+
+(defn generate-report [report-type]
+  (try
+    (let [out (java.io.ByteArrayOutputStream.)]
+      (condp = (keyword report-type)
+        :table (reports/table-report out)
+        :list (reports/list-report out))
+      (write-response (.toByteArray out)))
+    (catch Exception ex
+      (layout/render "home.html" {:error (.getMessage ex)}))))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (GET "/:report-type" [report-type] (generate-report report-type)))
 
