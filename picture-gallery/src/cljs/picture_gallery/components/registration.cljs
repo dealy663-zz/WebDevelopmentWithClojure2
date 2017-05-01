546270c5a6582eb0fc6c56dae9b8b484530ca219
Derek Ealy <derek.ealy@novawurks.com>
2017-05-01 09:50:00 -0700

    Added the register menu to the navbar, which pops up the registration dialog

--- picture-gallery/src/cljs/picture_gallery/components/registration.cljs
+++ picture-gallery/src/cljs/picture_gallery/components/registration.cljs
@@ -1,9 +1,27 @@
 (ns picture-gallery.components.registration
   (:require [reagent.core :refer [atom]]
+            [ajax.core :as ajax]
+            [reagent.session :as session]
+            [picture-gallery.validation :refer [registration-errors]]
             [picture-gallery.components.common :as c]))
 
+(defn register! [fields errors]
+  (reset! errors (registration-errors @fields))
+  (when-not @errors
+    (ajax/POST "/register"
+               {:params @fields
+                :handler
+                        #(do
+                           (session/put! :identity (:id @fields))
+                           (reset! fields {}))
+                :error-handler
+                        #(reset!
+                           errors
+                           {:server-error (get-in % [:response :message])})})))
+
 (defn registration-form []
-  (let [fields (atom {})]
+  (let [fields (atom {})
+        error (atom nil)]
     (fn []
       [c/modal
        [:div "Picture Gallery Registration"]
@@ -12,7 +30,18 @@
          [:strong " ✱ required field"]]
         [c/text-input "name" :id "enter a user name" fields]
         [c/password-input "password" :pass "enter a password" fields]
-        [c/password-input "password" :pass-confirm "re-enter the password" fields]]
+        [c/password-input "password" :pass-confirm "re-enter the password" fields]
+        (when-let [error (:server-error @error)]
+          [:div.alert.alert-danger error])]
        [:div
-        [:button.btn.btn-primary "Register"]
-        [:button.btn.btn-danger "Cancel"]]])))
+        [:button.btn.btn-primary
+         {:on-click #(register! fields error)}
+         "Register"]
+        [:button.btn.btn-danger
+         {:on-click #(session/remove! :modal)}
+         "Cancel"]]])))
+
+(defn registration-button []
+  [:a.btn
+   {:on-click #(session/put! :modal registration-form)}
+   "register"])
