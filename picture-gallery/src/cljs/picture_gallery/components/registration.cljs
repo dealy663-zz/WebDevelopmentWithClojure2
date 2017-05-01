9a98a02b1b1b5a30a60fb3bfe2cd4caff627cf5b
Derek Ealy <derek.ealy@novawurks.com>
2017-05-01 10:38:45 -0700

    Completed Task A with working registration popup

--- picture-gallery/src/cljs/picture_gallery/components/registration.cljs
+++ picture-gallery/src/cljs/picture_gallery/components/registration.cljs
@@ -13,7 +13,8 @@
                 :handler
                         #(do
                            (session/put! :identity (:id @fields))
-                           (reset! fields {}))
+                           (reset! fields {})
+                           (session/remove! :modal))
                 :error-handler
                         #(reset!
                            errors
@@ -29,7 +30,11 @@
         [:div.well.well-sm
          [:strong " ✱ required field"]]
         [c/text-input "name" :id "enter a user name" fields]
+        (when-let [error (first (:id @error))]
+          [:div.alert.alert-danger error])
         [c/password-input "password" :pass "enter a password" fields]
+        (when-let [error (first (:pass @error))]
+          [:div.alert.alert-danger error])
         [c/password-input "password" :pass-confirm "re-enter the password" fields]
         (when-let [error (:server-error @error)]
           [:div.alert.alert-danger error])]
