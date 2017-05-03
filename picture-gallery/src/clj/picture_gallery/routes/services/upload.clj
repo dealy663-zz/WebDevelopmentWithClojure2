1ed54df8aaf0b705b18a311585b412ced06e201c
Derek Ealy <derek.ealy@novawurks.com>
2017-05-03 16:09:20 -0700

    Completed Task B with working login popup

---
+++ picture-gallery/src/clj/picture_gallery/routes/services/upload.clj
@@ -0,0 +1,29 @@
+(ns picture-gallery.routes.services.upload
+  (:require [picture-gallery.db.core :as db]
+            [ring.util.http-response :refer :all]
+            [clojure.tools.logging :as log])
+  (:import [java.awt.image AffineTransformOp BufferedImage]
+           [java.io ByteArrayOutputStream FileInputStream]
+           java.awt.geom.AffineTransform
+           javax.imageio.ImageIO
+           java.net.URLEncoder))
+
+(defn scale-image [file thumb-size])
+(defn image->byte-array [image])
+
+(defn save-image! [user {:keys [tempfile filename content-type]}]
+  (try
+    (let [db-file-name (str user (.replaceAll filename "[^a-zA-Z0-9-_\\.]" ""))]
+      (db/save-file! {:owner user
+                      :type content-type
+                      :name db-file-name
+                      :data (file->byte-array tempfile)})
+      (db/save-file! {:owner user
+                      :type "image/png"
+                      :data (image->byte-array
+                              (scale-image tempfile thumb-size))
+                      :name (str thumb-prefix db-file-name)}))
+    (ok {:result :ok})
+    (catch Exception e
+      (log/error e)
+      (internal-server-error "error"))))
