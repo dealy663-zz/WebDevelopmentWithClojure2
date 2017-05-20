9782a5f08bb9effd36c1ad777a884565ea52b6d5
Derek Ealy <dealy663@gmail.com>
2017-05-20 10:14:47 -0700

    Added AlbumColors js library

--- picture-gallery/src/cljs/picture_gallery/components/gallery.cljs
+++ picture-gallery/src/cljs/picture_gallery/components/gallery.cljs
@@ -3,7 +3,8 @@
             [reagent.session :as session]
             [ajax.core :as ajax]
             [clojure.string :as s]
-            [picture-gallery.components.common :as c]))
+            [picture-gallery.components.common :as c]
+            [reagent.core :as reagent]))
 
 (defn partition-links [links]
   (when (not-empty links)
@@ -38,12 +39,37 @@
            :class (when (= @page (dec pages)) "disabled")}
           [:span "»"]]]))))
 
-(defn image-modal [link]
+(defn rgb-str [[r g b] mask]
+  (str "rgba(" r "," g "," b "," mask ")"))
+
+(defn set-background! [style [c1 c2 c3]]
+  (set! (.-background style)
+        (str "linear-gradient("
+             (rgb-str c3 0.8) ","
+             (rgb-str c2 0.9) ","
+             (rgb-str c1 1) ")")))
+
+(defn image-panel-did-mount [thumb-link]
+  (fn [div]
+    (.getColors
+      (js/AlbumColors. thumb-link)
+      (fn [colors]
+        (-> div reagent/dom-node .-style (set-background! colors))))))
+
+(defn render-image-panel [link]
+  (fn []
+    [:img.image.panel.panel-default
+     {:on-click #(session/remove! :modal)
+      :src link}]))
+
+(defn image-panel [thumb-link link]
+  (reagent/create-class {:render (render-image-panel link)
+                         :component-did-mount (image-panel-did-mount thumb-link)}))
+
+(defn image-modal [thumb-link link]
   (fn []
     [:div
-     [:img.image.panel.panel-default
-      {:on-click #(session/remove! :modal)
-       :src link}]
+     [image-panel thumb-link link]
      [:div.modal-backdrop.fade.in]]))
 
 (declare gallery)
@@ -92,9 +118,10 @@
     {:src (str js/context "/gallery/" owner "/" name)
      :on-click #(session/put!
                   :modal
-                  (image-modal (str js/context
-                                    "/gallery/" owner "/"
-                                    (s/replace name #"thumb_" ""))))}]
+                  (image-modal
+                    (str js/context "/gallery/" owner "/" name)
+                    (str js/context "/gallery/" owner "/"
+                         (s/replace name #"thumb_" ""))))}]
    (when (= (session/get :identity) owner)
      [:div.text-xs-center>div.btn.btn-danger
       {:on-click #(delete-image-button owner name)}
